6cbb934536e7a7d19ecc6ca5e934bb45d01bf9d7
Shotgun <daniel@llama.nu>
2015-10-17 15:40:40 +0200

    Added Information

--- installdwb.sh
+++ installdwb.sh
@@ -1,4 +1,5 @@
 # dwb installation requires git and make
+# I made this script because the package in archlinux repositories are outdated
 
 sudo pacman -S git make webkitgtk2
 mkdir ~/dwbbuild && cd ~/dwbbuild && git clone https://www.bitbucket.org/portix/dwb.git && cd dwb && sudo make install 
@@ -6,3 +7,9 @@ rm -r ~/dwbbuild
 
 # Optional
 # sudo pacman -S gst-plugins-base gst-plugins-good gst-libav
+
+# Recomended programs
+#	mutt
+#	nano
+#	rtorrent
+#	lftp
